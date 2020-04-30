c3cc89607da7e081eb5728d1043b368a99f540e5
James H Hill <jhill1906@gmail.com>
2020-04-30 13:04:07 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pluralsight.BegCshCollections.ReadAllCountries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\jhill\source\data\CountryList.csv";
+            CsvReader reader = new CsvReader(filePath);
+
+            List<Country> countries = reader.ReadAllCountries();
+
+            foreach(Country country in countries)
+            {
+                Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)} : {country.Name}");
+            }
+
+            Console.WriteLine($"{countries.Count} countries");
+        }
+    }
+}
